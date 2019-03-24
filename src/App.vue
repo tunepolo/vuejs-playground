95df20d3fcf3b9676c96df9d11ea47ec82cbc416
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-03-24 14:27:46 +0900

    別ファイルでヘッダを追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <myheader></myheader>
     <p v-if="msg.length > 0">
       {{msg}}
     </p>
@@ -12,7 +13,12 @@
 </template>
 
 <script>
+import myheader from './components/myheader'
+
 export default {
+  components: {
+    myheader
+  },
   data () {
     return {
       msg: "Hello World!"
