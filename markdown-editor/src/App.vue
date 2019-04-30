f089f195f1fedc062a90def44d80d976d34f389f
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-04-30 17:59:58 +0900

    prettierで指摘された点を修正

--- markdown-editor/src/App.vue
+++ markdown-editor/src/App.vue
@@ -7,38 +7,41 @@
 </template>
 
 <script>
-import _ from 'lodash'
-import marked from 'marked'
+import _ from "lodash";
+import marked from "marked";
 
 export default {
   name: "editor",
   data: function() {
-    return{
-      input: '# hello'
-    }
+    return {
+      input: "# hello"
+    };
   },
   computed: {
     compiledMarkdown: function() {
-      return marked(this.input, {sanitize: true})
+      return marked(this.input, { sanitize: true });
     }
   },
   methods: {
-    update: _.debounce(function (e) {
-      this.input = e.target.value
+    update: _.debounce(function(e) {
+      this.input = e.target.value;
     }, 300)
   }
 };
 </script>
 
 <style>
-html, body, #editor {
+html,
+body,
+#editor {
   margin: 0;
   height: 100%;
-  font-family: 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Helvetica Neue", Arial, sans-serif;
   color: #333;
 }
 
-textarea, #editor div {
+textarea,
+#editor div {
   display: inline-block;
   width: 49%;
   height: 100%;
@@ -54,7 +57,7 @@ textarea {
   outline: none;
   background-color: #f6f6f6;
   font-size: 14px;
-  font-family: 'Monaco', courier, monospace;
+  font-family: "Monaco", courier, monospace;
   padding: 20px;
 }
 
